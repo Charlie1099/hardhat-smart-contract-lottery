4420fe98fb2a0c4a5fd97a14adec703443d23ee4
charlie1099 <cdcharlesworth@gmail.com>
2022-09-23 16:40:52 -0400

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("")
+
+
+
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+};
